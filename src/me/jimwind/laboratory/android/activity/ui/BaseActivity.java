06bfa15fdc01b5c96addb7fc46d77bea49c326db
jimwind <jimwindgao@gmail.com>
2015-12-17 15:05:14 +0800

    create my laboratory project

---
+++ src/me/jimwind/laboratory/android/activity/ui/BaseActivity.java
@@ -0,0 +1,18 @@
+package me.jimwind.laboratory.android.activity.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+
+/**
+ * @Description
+ * @author mi.gao
+ * @date  2015-12-17
+ */
+public abstract class BaseActivity extends FragmentActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ src/me/jimwind/laboratory/android/activity/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package me.jimwind.laboratory.android.activity.ui;
+
+import me.jimwind.laboratory.R;
+import android.os.Bundle;
+
+/**
+ * @Description
+ * @author mi.gao
+ * @date  2015-12-17
+ */
+public class MainActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
